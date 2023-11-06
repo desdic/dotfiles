172b08e50c40121b981e071ad86a063d7fc22930
Kim Gert Nielsen <kn@insecurity.dk>
2023-11-06 13:17:13 +0100

    Path to config might be needed but it works

--- bin/executable_newsession_zellij.sh
+++ bin/executable_newsession_zellij.sh
@@ -9,7 +9,7 @@ pgrep -a foot|grep -q zellij
 case $? in 
   1)
     # sleep 1 hack to make zellij get correct size of window
-    /sbin/foot -a work zsh --login -c 'sleep 1;zellij attach --create work'
+    /sbin/foot -a work zsh --login -c 'sleep 1;zellij --config ~/.config/zellij/config.kdl attach --create work'
     ;;
   *)
     ${ZELLIJ} --session work action new-tab
