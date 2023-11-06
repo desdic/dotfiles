d82affae73bd547a911b509318637ebdf4c225de
Kim Gert Nielsen <kn@insecurity.dk>
2023-11-06 11:44:42 +0100

    Added session with zellij

---
+++ bin/executable_newsession_zellij.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ZELLIJ=/usr/bin/zellij
+
+[ ! -x "${ZELLIJ}" ] && exit 1
+
+pgrep -a foot|grep -q zellij
+
+case $? in 
+  1)
+    # sleep 1 hack to make zellij get correct size of window
+    /sbin/foot -a work zsh --login -c 'sleep 1;zellij attach --create work'
+    ;;
+  *)
+    ${ZELLIJ} --session work action new-tab
+    ;;
+esac
