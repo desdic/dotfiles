90c7c1c9aeb58c8049654a7378fad635d087a30e
Kim Gert Nielsen <kn@insecurity.dk>
2023-11-15 21:04:33 +0100

    Generate colors with pywal

--- dot_config/hypr/executable_changewallpaper.sh
+++ dot_config/hypr/executable_changewallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WALLPAPERPATH=~/Pictures/wallpapers
-
-WALLPAPER=$(find ${WALLPAPERPATH}|sort -R|tail -1)
-
-/sbin/swww img "${WALLPAPER}"
+WALLPAPER=$(find ${WALLPAPERPATH} -type f -iname '*.jpg'|sort -R|tail -1)
+cp "${WALLPAPER}" ~/.cache/current_wallpaper.jpg
+/sbin/wal -q -s -t -i ~/.cache/current_wallpaper.jpg
+/sbin/swww img ~/.cache/current_wallpaper.jpg
