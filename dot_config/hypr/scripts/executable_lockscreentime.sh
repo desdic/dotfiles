23cfdfa9bcab81d101e26ab062b25a8f2a5b6ff2
Kim Gert Nielsen <kn@insecurity.dk>
2023-12-07 07:20:51 +0100

    change wallpaper after sleep

--- dot_config/hypr/scripts/executable_lockscreentime.sh
+++ dot_config/hypr/scripts/executable_lockscreentime.sh
@@ -4,7 +4,7 @@ timeswaylock=600
 timeoff=660
 
 if [ -f "/usr/bin/swayidle" ]; then
-    /usr/bin/swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock -f'
+    /usr/bin/swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' before-sleep 'swaylock -f' after-resume "$HOME/.config/hypr/scripts/changewallpaper.sh"
 else
     echo "swayidle not installed."
 fi;
