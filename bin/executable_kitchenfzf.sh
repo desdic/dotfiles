d80b7ff07aaf5efbc44f46039a4601da4989b2f2
Kim Gert Nielsen <kn@insecurity.dk>
2023-12-16 09:57:48 +0100

    Only run if .kitchen.yml exists

--- bin/executable_kitchenfzf.sh
+++ bin/executable_kitchenfzf.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-suites=( $(kitchen list --bare 2>/dev/null|tr '\n' ' ') )
-targets=$1
-if [ -z "$1" ]; then
-  targets=( converge test login verify destroy )
-else
-  targets=("$1")
-fi
+if [ -f .kitchen.yml ]; then
+  suites=( $(kitchen list --bare 2>/dev/null|tr '\n' ' ') )
+  targets=$1
+  if [ -z "$1" ]; then
+    targets=( converge test login verify destroy )
+  else
+    targets=("$1")
+  fi
 
-list=()
-for key in "${suites[@]}"
-do
-  for target in "${targets[@]}"
+  list=()
+  for key in "${suites[@]}"
   do
-    list+=("kitchen $target $key")
+    for target in "${targets[@]}"
+    do
+      list+=("kitchen $target $key")
+    done
   done
-done
 
-printf -v joined '%s,' "${list[@]}"
-choice=$(echo "$joined"|tr ',' '\n'|fzf +m)
-if [ -n "$choice" ]; then
-  eval "$choice"
+  printf -v joined '%s,' "${list[@]}"
+  choice=$(echo "$joined"|tr ',' '\n'|fzf +m)
+  if [ -n "$choice" ]; then
+    eval "$choice"
+  fi
 fi
