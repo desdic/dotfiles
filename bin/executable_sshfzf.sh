bc89faab8feb589adb645aca114c53d39f40d920
Kim Gert Nielsen <kn@insecurity.dk>
2023-12-18 06:23:54 +0100

    cache sshfzf

--- bin/executable_sshfzf.sh
+++ bin/executable_sshfzf.sh
@@ -1,16 +1,15 @@
-selected_host=$({ ~/git/chef-repo/scripts/cnodes & awk '{print $1}' ~/.ssh/known_hosts; }|sort|uniq|fzf +m --query "$LBUFFER" --prompt="SSH remote > " --color "prompt:#89b4fa")
-if [ -z "$selected_host" ]; then
-  exit 0
+selected_host=$(cat $HOME/tmp/sshfzf.cache|sort|uniq|fzf +m --query "$LBUFFER" --prompt="SSH remote > " --color "prompt:#89b4fa")
+if [ -n "$selected_host" ]; then
+  if [ -n "$TMUX" ]; then
+    WINDOWNAME=$(echo "${selected_host}"|cut -d . -f1)
+    tmux rename-window -t work "${WINDOWNAME}"
+  elif [ -n "$ZELLIJ" ]; then
+    zellij action rename-tab "${selected_host}"
+    zellij action rename-pane "${selected_host}"
+  fi
+
+  # add to zsh history
+  print -s ssh "${selected_host}"
+  ssh "${selected_host}"
 fi
 
-if [ -n "$TMUX" ]; then
-  WINDOWNAME=$(echo "${selected_host}"|cut -d . -f1)
-  tmux rename-window -t work "${WINDOWNAME}"
-elif [ -n "$ZELLIJ" ]; then
-  zellij action rename-tab "${selected_host}"
-  zellij action rename-pane "${selected_host}"
-fi
-
-# add to zsh history
-print -s ssh "${selected_host}"
-ssh "${selected_host}"
