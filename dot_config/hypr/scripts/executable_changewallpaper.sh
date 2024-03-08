21271868f1b11cc731c67b672309e207e50469ae
Kim Gert Nielsen <kn@insecurity.dk>
2024-03-08 11:17:28 +0100

    Convert image for hyprlock

--- dot_config/hypr/scripts/executable_changewallpaper.sh
+++ dot_config/hypr/scripts/executable_changewallpaper.sh
@@ -3,5 +3,6 @@
 WALLPAPERPATH=~/Pictures/wallpapers
 WALLPAPER=$(find ${WALLPAPERPATH} -type f -iname '*.jpg'|sort -R|tail -1)
 cp "${WALLPAPER}" ~/.cache/current_wallpaper.jpg
+/sbin/convert ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.png&
 /sbin/wal -q -s -t -i ~/.cache/current_wallpaper.jpg
 /sbin/swww img ~/.cache/current_wallpaper.jpg
