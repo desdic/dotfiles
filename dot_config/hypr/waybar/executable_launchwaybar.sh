d2179043176b442d0d7e3aa4aaff1a8db16f9e18
Kim Gert Nielsen <kn@insecurity.dk>
2024-03-28 08:12:05 +0100

    Reload waybar on login

--- dot_config/hypr/waybar/executable_launchwaybar.sh
+++ dot_config/hypr/waybar/executable_launchwaybar.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "1" > $HOME/.cache/waybar.reload
 
-CONFIG_FILES="$HOME/.config/hypr/waybar/config $HOME/.config/hypr/waybar/style.css /$HOME/.cache/wal/colors-waybar.css"
+CONFIG_FILES="$HOME/.config/hypr/waybar/config $HOME/.config/hypr/waybar/style.css $HOME/.cache/wal/colors-waybar.css $HOME/.cache/waybar.reload"
 
 trap "killall waybar" EXIT
 
