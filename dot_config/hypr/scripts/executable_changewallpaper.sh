6bf807df9548b5f464bce1326bcd99f35c8ee473
Kim Gert Nielsen <kn@insecurity.dk>
2024-04-07 20:58:04 +0200

    Move to swww-daemon

--- dot_config/hypr/scripts/executable_changewallpaper.sh
+++ dot_config/hypr/scripts/executable_changewallpaper.sh
@@ -5,4 +5,5 @@ WALLPAPER=$(find ${WALLPAPERPATH} -type f -iname '*.jpg'|sort -R|tail -1)
 cp "${WALLPAPER}" ~/.cache/current_wallpaper.jpg
 /sbin/convert ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.png&
 /sbin/wal -q -s -t -i ~/.cache/current_wallpaper.jpg
+/sbin/swww query || /sbin/swww-daemon
 /sbin/swww img ~/.cache/current_wallpaper.jpg
