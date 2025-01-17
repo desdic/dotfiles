d7e99e13cf0c462e9db0179686623b012ae51924
Kim Gert Nielsen <kn@insecurity.dk>
2025-01-17 16:45:15 +0100

    Added session with ghostty

--- bin/executable_newsession.sh
+++ bin/executable_newsession.sh
@@ -2,18 +2,35 @@
 
 TMUX=/usr/bin/tmux
 HC=/usr/bin/hyprctl
-TERM=foot
+WEZTERM=/sbin/wezterm
+FOOT=/sbin/foot
+ALACRITTY=/sbin/alacritty
+KITTY=/sbin/kitty
+GHOSTTY=/sbin/ghostty
 
 [ ! -x ${TMUX} ] && exit 1
 
+if [ -x ${GHOSTTY} ]; then
+  ATTACH="${GHOSTTY} --class=my.work -e ${TMUX} attach -t work"
+elif [ -x ${KITTY} ]; then
+  ATTACH="${KITTY} --class my.work ${TMUX} attach -t work"
+elif [ -x ${WEZTERM} ]; then
+  ATTACH="${WEZTERM} start --class my.work --new-tab ${TMUX} attach -t work"
+elif [ -x ${FOOT} ]; then
+  ATTACH="${FOOT} -a my.work ${TMUX} attach -t work"
+elif [ -x ${ALACRITTY} ]; then
+  ATTACH="${ALACRITTY} --class my.work -e ${TMUX} attach -t work"
+fi
+
+
 function focus() {
-  WS=$(${HC} clients -j|jq -r '.[]| select(.class=="work")|.workspace.id')
+  WS=$(${HC} clients -j|jq -r '.[]| select(.class=="my.work")|.workspace.id')
   if [ -n "${WS}" ]; then
     ${HC} dispatch workspace "${WS}"
 
     TRIES=0
     ACTIVE=$(${HC} activewindow -j|jq -r '.initialClass')
-    while [[ "${ACTIVE}" != "work" ]]
+    while [[ "${ACTIVE}" != "my.work" ]]
     do
       TRIES=$((TRIES + 1))
       if [ $TRIES -gt 10 ]; then
@@ -25,28 +42,21 @@ function focus() {
   fi
 }
 
-LANG=da_US.UTF-8
+# LANG=da_US.UTF-8
 
 RES=$(${TMUX} ls -F "#{session_name}_#{?session_attached,attached,not_attached}"|grep ^work_)
+echo ${RES}
 
 case ${RES} in
   work_attached)
     focus
-    ${TMUX} -2 new-window -a -t work
+    ${TMUX} new-window -t work
     ;;
   work_not_attached)
-    if [ "${TERM}" = "foot" ]; then
-      /sbin/foot -a work ${TMUX} attach -t work
-    elif [ "${TERM}" = "alacritty" ]; then
-      /sbin/alacritty --class work -e ${TMUX} attach -t work
-    fi
+    ${ATTACH}
     ;;
   *)
     ${TMUX} new-session -s work -d
-    if [ "${TERM}" = "foot" ]; then
-      /sbin/foot -a work ${TMUX} attach -t work
-    elif [ "${TERM}" = "alacritty" ]; then
-      /sbin/alacritty --class work -e ${TMUX} attach -t work
-    fi
+    ${ATTACH}
     ;;
 esac
