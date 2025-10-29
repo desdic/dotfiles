780db32b2dff9c59b545ead4ab42d9d692487814
Kim Gert Nielsen <kn@insecurity.dk>
2025-10-29 08:45:43 +0100

    generalize container fzf

---
+++ dot_config/zsh/container.zsh
@@ -0,0 +1,52 @@
+container_fzf() {
+	local cmds=()
+
+	# handle kitchen builds
+	KITCHENDIR=
+	if [ -e kitchen-build/.kitchen ]; then
+	  KITCHENDIR=kitchen-build/.kitchen
+	elif [ -e .kitchen ]; then
+	  KITCHENDIR=.kitchen
+	fi
+
+	# handle kitchen in cookbooks
+	if [[ -f .kitchen.yml ]]; then
+		cmds+=("kitchen destroy")
+		while read -r entry; do
+			cmds+=("kitchen destroy $entry")
+			cmds+=("kitchen converge $entry")
+			cmds+=("kitchen verify $entry")
+			cmds+=("kitchen test $entry")
+			cmds+=("kitchen login $entry")
+		done < <(kitchen list --bare)
+	fi
+
+	if [ -n "${KITCHENDIR}" ]; then
+		while read -r entry; do
+			KHOST=$(awk '/^hostname:/{print $2}' "$entry")
+			KPORT=$(awk '/^port:/{print $2}' "$entry")
+			KUSERNAME=$(awk '/^username:/{print $2}' "$entry")
+			KKEY=$(awk -F '"' '/^ssh_key:/{print $2}' "$entry")
+			cmds+=("ssh -o StrictHostKeyChecking=no \"$KUSERNAME\"@\"$KHOST\" -i \"$KKEY\" -p \"$KPORT\"")
+		done < <(find "${KITCHENDIR}" -iname '*.yml')
+	fi
+
+	# handle docker
+	if [[ -f docker-compose.yml ]]; then
+		while read -r entry; do
+			cmds+=("docker exec -it $entry /bin/bash")
+		done < <(docker ps --format '{{.Names}}')
+	fi
+
+
+	local selected
+	selected=$(printf "%s\n" "${cmds[@]}" | fzf -m --prompt="cmd > " --color "prompt:#89b4fa" --query="$1") || return
+
+	# Execute selected commands in main shell
+	for cmd in ${(f)selected}; do
+		echo "🚀 Executing: $cmd"
+		print -s "$cmd"
+		fc -W
+		eval "$cmd"
+	done
+}
