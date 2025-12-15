c3a56ef70ab41dfd1334da9b7d44e1cec5c16792
Kim Gert Nielsen <kn@insecurity.dk>
2025-12-15 10:32:44 +0100

    Split zsh config and make edit more vim like

---
+++ dot_config/zsh/vimode.zsh
@@ -0,0 +1,41 @@
+# Keyboard bindings
+# bindkey -e
+bindkey -v
+export KEYTIMEOUT=1
+bindkey -r "\e'"
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
+
+bindkey -M vicmd '?' history-incremental-search-backward
+bindkey -M vicmd '/' history-incremental-search-forward
+bindkey '^R' history-incremental-search-backward
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
+export VI_MODE_SET_CURSOR=true
+
+function zle-keymap-select {
+	if [[ ${KEYMAP} == vicmd ]]; then
+		echo -ne '\e[2 q' # Block
+	else
+		echo -ne '\e[6 q' # Beam
+	fi
+}
+zle -N zle-keymap-select
+
+function zle-linie-init {
+	zle -K viins
+	echo -ne '\e[6 q' # Beam
+}
+
+zle -N zle-linie-init
+
+function vi-yank-clipboard {
+	zle vi-yank
+	echo "$CUTBUFFER"|wl-copy -p
+}
+
+zle -N vi-yank-clipboard
+bindkey -M vicmd 'y' vi-yank-clipboard
