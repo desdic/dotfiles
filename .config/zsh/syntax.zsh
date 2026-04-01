36d12e921f7f4df161725097b17c014fd4b6e031
Kim Gert Nielsen <kn@insecurity.dk>
2026-04-01 19:38:16 +0200

    check paths

--- .config/zsh/syntax.zsh
+++ .config/zsh/syntax.zsh
@@ -1,1 +1,5 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -f /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+	source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+	source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi
