9f2658614402add5e1b11a9d3688cd6a385a40d1
Kim Nielsen <kn@insecurity.dk>
2026-08-01 19:31:22 +0200

    Add herdr

--- bin/fzfcd.sh
+++ bin/fzfcd.sh
@@ -1,14 +1,19 @@
 # there should be no !# since we source this script to add it to our history
-DIR=$(fd . --type directory --type symlink ~/ ~/.config ~/git ~/src/private --maxdepth 1|fzf +m --prompt="directory > " --query="$1")
+DIR=$(fd . --type directory --type symlink ~/ ~/.config ~/git ~/src/private --maxdepth 1|fzf +m --select-1 --prompt="directory > " --query="$1")
 if [ -n "$DIR" ]; then
 
-  NAME=$(basename $DIR)
-  if [ -n "$TMUX" ]; then
-    tmux rename-window "${NAME}"
-  fi
+	if [ -n "$DIR" ]; then
+		cd $DIR
+	fi
 
-  if [ -n "$DIR" ]; then
-    cd $DIR
-  fi
+	echo "hello"
+	if [ -n "$TMUX" ]; then
+		SHORT=$(echo $PWD|rev|cut -d '/' -f-2|rev|tr . _)
+		tmux rename-window -- "${SHORT}"
+	elif [ -n "$HERDR_ENV" ]; then
+		SHORT=$(echo $PWD|rev|cut -d '/' -f-2|rev)
+		herdr pane rename "${HERDR_PANE_ID}" "${SHORT}" > /dev/null
+		herdr tab rename "${HERDR_TAB_ID}" "${SHORT}" > /dev/null
+	fi
 
 fi
